69b78b40cbaa4fa6396660ec4de5b2b21e0a8bd6
Razer <vietchelsea2k1@gmail.com>
2023-04-30 18:47:00 +0700

    db

--- src/configs/connerDB.js
+++ src/configs/connerDB.js
@@ -1,7 +1,8 @@
 import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
+    host: 'db4free.net',
+    user: 'selling_flower',
+    password: 'Viet29112001',
     port: 3306,
     database: 'selling_flower'
 })
